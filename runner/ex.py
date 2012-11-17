08f7c92bacc6d73a2cde9d220cd789ff64f35585
vaikuntj <vaikuntj@Jagss-MacPro.local>
2012-11-18 00:28:17 +0530

    Added TestRunner & auto-detect of test cases for execution.

--- runner/ex.py
+++ runner/ex.py
@@ -1,7 +1,9 @@
 from _src.utilities import Utilities
-from tests.tests2.test3.tests4.dec_class import  AssertTests
+from tests.dec import  AssertTests
 #from tests.dec import AssertTests
 
 if __name__ == "__main__" :
     t = AssertTests()
-    k=Utilities.runTests(t)
+    Utilities.runTests(t)
+    print "hi"
+    
--- runner/runner.py
+++ runner/runner.py
@@ -1,31 +1,56 @@
 import os
 from _src.constants import Constants
 import tokenize
+from _src.utilities import Utilities
 
-def get_directories(path):
-    discovered_directories = []
-    for component in os.listdir(path):
-        if os.path.isdir(Constants.REFERENCE_DIR_PATH + os.sep + component) and component not in Constants.RESERVED_DIRECTORIES:
-            discovered_directories.append(component)
-    return discovered_directories
-
-def get_class_name(token_str):
-    with open(r + os.sep + test_file) as file_obj:
-        tokens = tokenize.generate_tokens(file_obj.readline)
-        for _, token, _, _,source_line in tokens:
-            if token == 'class':
-                class_name=source_line.strip().split(' ')[1]
-                common_name_pos = class_name.find(Constants.TEST_CASE_CLASS_SUFFIX)
-                if common_name_pos >= 0:
-                    return class_name[:common_name_pos]+Constants.TEST_CASE_CLASS_SUFFIX
-        file_obj.close()
-
-
-if __name__ == "__main__" :
+class TestRun:
     
-    for directory in get_directories(Constants.REFERENCE_DIR_PATH):
-        for r, directories, test_files in os.walk(Constants.REFERENCE_DIR_PATH + os.sep + directory):
-            for test_file in test_files:
-                file_name, file_extension = os.path.splitext(test_file)
-                if file_extension in Constants.TEST_FILE_EXTENSION and file_name not in Constants.IGNORE_FILE_NAME:
-                    print get_class_name(r+os.sep+test_file)
+    @classmethod
+    def get_directories(cls,path):
+        discovered_directories = []
+        for component in os.listdir(path):
+            if os.path.isdir(Constants.REFERENCE_DIR_PATH + os.sep + component) and component not in Constants.RESERVED_DIRECTORIES:
+                discovered_directories.append(component)
+        return discovered_directories
+
+    @classmethod
+    def get_class_name(cls,file_path):
+        with open(file_path) as file_obj:
+            tokens = tokenize.generate_tokens(file_obj.readline)
+            for _, token, _, _, source_line in tokens:
+                if token == 'class':
+                    class_name = source_line.strip().split(' ')[1]
+                    common_name_pos = class_name.find(Constants.TEST_CASE_CLASS_SUFFIX)
+                    if common_name_pos >= 0:
+                        class_name=class_name[:common_name_pos] + Constants.TEST_CASE_CLASS_SUFFIX
+            file_obj.close()
+        return class_name
+
+    @classmethod
+    def get_test_class(cls,test_path,file_name,test_class_name):
+        test_path_list=test_path.split(os.sep)
+        ref_list = Constants.REFERENCE_DIR_PATH.split(os.sep)
+        final_referenced_list=[]
+    
+        for each_path in test_path_list:
+            if each_path not in  ref_list:
+                final_referenced_list.append(each_path)
+        final_referenced_list.append(file_name)
+        import_stmt=".".join(final_referenced_list)
+        import_handle=__import__(import_stmt)        
+   
+        for each_import in final_referenced_list[1:len(final_referenced_list)]:
+            import_handle=getattr(import_handle,each_import)
+        test_class_import_reference=getattr(import_handle,test_class_name)
+        return test_class_import_reference
+
+    @classmethod    
+    def executeTestRun(cls):    
+        for directory in cls.get_directories(Constants.REFERENCE_DIR_PATH):
+            for r, _, test_files in os.walk(Constants.REFERENCE_DIR_PATH + os.sep + directory):
+                for test_file in test_files:
+                    file_name, file_extension = os.path.splitext(test_file)
+                    if file_extension in Constants.TEST_FILE_EXTENSION and file_name not in Constants.IGNORE_FILE_NAME:
+                        test_class_name = cls.get_class_name(r + os.sep + test_file)
+                        test_class_instance=cls.get_test_class(r, file_name, test_class_name)()
+                        Utilities.runTests(test_class_instance)
