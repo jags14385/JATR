2d883b3255c4917d396ff43980a84e427c0f4b49
vaikuntj <vaikuntj@Jagss-MacPro.local>
2012-11-17 15:37:25 +0530

    Added TestFile & Class Discovery . Added IGNORE_FILE_NAME & TEST_FILE_EXTENSIONS to Constants.

--- _src/constants.py
+++ _src/constants.py
@@ -4,5 +4,6 @@ class Constants:
     TEST_CASE_CLASS_SUFFIX = "Tests"
     TEST_CASE_FIXTURE_SUFFIX = "Fixture"
     REFERENCE_DIR_PATH="/Users/vaikuntj/Work/Testing/JATR"
-    RESERVED_DIRECTORIES=['.git','runner','_src']
-    PATTERN="*.py"
+    RESERVED_DIRECTORIES=['.git','runner','_src','__pycache__']
+    TEST_FILE_EXTENSION=[".py"]
+    IGNORE_FILE_NAME=['__init__']
--- runner/runner.py
+++ runner/runner.py
@@ -1,28 +1,31 @@
 import os
 from _src.constants import Constants
+import tokenize
 
 def get_directories(path):
-    discovered_directories=[]
+    discovered_directories = []
     for component in os.listdir(path):
-        if os.path.isdir(Constants.REFERENCE_DIR_PATH+os.sep+component) and component not in Constants.RESERVED_DIRECTORIES:
+        if os.path.isdir(Constants.REFERENCE_DIR_PATH + os.sep + component) and component not in Constants.RESERVED_DIRECTORIES:
             discovered_directories.append(component)
     return discovered_directories
+
+def get_class_name(token_str):
+    with open(r + os.sep + test_file) as file_obj:
+        tokens = tokenize.generate_tokens(file_obj.readline)
+        for _, token, _, _,source_line in tokens:
+            if token == 'class':
+                class_name=source_line.strip().split(' ')[1]
+                common_name_pos = class_name.find(Constants.TEST_CASE_CLASS_SUFFIX)
+                if common_name_pos >= 0:
+                    return class_name[:common_name_pos]+Constants.TEST_CASE_CLASS_SUFFIX
+        file_obj.close()
 
 
 if __name__ == "__main__" :
     
-    print get_directories(Constants.REFERENCE_DIR_PATH)
-    
-#        if os.path.isdir(component):
-#            print os.path.dirname(component)
-#        if component not in Constants.RESERVED_DIRECTORIES:
-#            abs_path=Constants.REFERENCE_DIR_PATH+os.sep+component
-#            if os.path.isdir(abs_path):
-#                discovered_directories.append(component)
-#    print discovered_directories     
-#    for path,directory,files in os.walk(Constants.REFERENCE_DIR_PATH):
-#        print directory
-#        for directory in discovered_directories:
-#            for name in files:
-#                if fnmatch(name, Constants.PATTERN):
-#                    print os.path.join(path, name)
+    for directory in get_directories(Constants.REFERENCE_DIR_PATH):
+        for r, directories, test_files in os.walk(Constants.REFERENCE_DIR_PATH + os.sep + directory):
+            for test_file in test_files:
+                file_name, file_extension = os.path.splitext(test_file)
+                if file_extension in Constants.TEST_FILE_EXTENSION and file_name not in Constants.IGNORE_FILE_NAME:
+                    print get_class_name(r+os.sep+test_file)
